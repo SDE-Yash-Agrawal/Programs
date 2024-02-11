f860e2edf2a7bae02961502e28665609e09ac7e4
SDE Yash Agrawal <sde.yash.agrawal@gmail.com>
2024-02-11 20:10:14 +0530

    New Pattern Problems are Added in Pyramid of Numbers using Java!

--- PyramidOfNumbers.java
+++ PyramidOfNumbers.java
@@ -7,7 +7,17 @@ public class PyramidOfNumbers
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		Func1(n);
+		System.out.println();
 		Func2(n);
+		System.out.println();
+		Func3(n);
+		System.out.println();
+		Func4(n);
+		System.out.println();
+		Func5(n);
+		System.out.println();
+		Func6(n);
+		System.out.println();
 	}
 
 	static void Func1(int n)
@@ -72,6 +82,154 @@ public class PyramidOfNumbers
 			    1 2 3 4 5 6 7
 			   1 2 3 4 5 6 7 8
 			  1 2 3 4 5 6 7 8 9
+
+    	*/
+	}
+
+	static void Func3(int n)
+	{
+		int col = 1;
+		for(int i=n; i>0; i--)
+		{
+			for(int j=i; j>1; j--)
+			{
+				System.out.print(" ");
+			}
+
+			for(int k=1; k<col+1; k++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+			col++;
+		}
+
+		/* Input - 9
+
+	       Output -   *
+				     * *
+				    * * *
+				   * * * *
+				  * * * * *
+				 * * * * * *
+				* * * * * * *
+			   * * * * * * * *
+			  * * * * * * * * *
+
+    	*/
+	}
+
+	static void Func4(int n)
+	{
+		int col = 1;
+		for(int i=n; i>0; i--)
+		{
+			for(int j=i*2; j>1; j--) //Here I have made j = i*2 because of equal space distribution.
+			{
+				System.out.print(" ");
+			}
+
+			for(int k=1; k<=col; k++)
+			{
+				System.out.print(k + " ");
+			}
+
+			for(int l=col-1; l>=1; l--)
+			{
+				System.out.print(l + " ");
+			}
+
+			System.out.println();
+			col++;
+		}
+
+		/* Input - 9
+
+	       Output -                    1
+						             1 2 1
+						           1 2 3 2 1
+						         1 2 3 4 3 2 1
+						       1 2 3 4 5 4 3 2 1
+						     1 2 3 4 5 6 5 4 3 2 1
+						   1 2 3 4 5 6 7 6 5 4 3 2 1
+						 1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+					   1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
+
+    	*/
+	}
+
+	static void Func5(int n)
+	{
+		int row = n;
+		for(int i=0 ; i<n; i++)
+		{
+			for(int j=0; j<i*2; j++)
+			{
+				System.out.print(" ");
+			}
+
+			for(int k=1; k<=row; k++)
+			{
+				System.out.print(k + " ");
+			}
+
+			for(int l= row-1; l>0; l--)
+			{
+				System.out.print(l + " ");
+			}
+			System.out.println();
+			row--;
+		}
+
+		/* Input - 9
+
+	       Output -  1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
+				       1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+				         1 2 3 4 5 6 7 6 5 4 3 2 1
+				           1 2 3 4 5 6 5 4 3 2 1
+				             1 2 3 4 5 4 3 2 1
+				           	   1 2 3 4 3 2 1
+				                 1 2 3 2 1
+				              	   1 2 1
+				                     1
+
+    	*/
+	}
+
+	static void Func6(int n)
+	{
+		int row = n;
+		for(int i=n; i>0; i--)
+		{
+			for(int j=i*2; j>1; j--)
+			{
+				System.out.print(" ");
+			}
+
+			for(int k=row; k<n+1; k++)
+			{
+				System.out.print(k + " ");
+			}
+
+			for(int l=n-1; l>=row; l--)
+			{
+				System.out.print(l + " ");
+			}
+			System.out.println();
+			row--;
+		}
+
+		/* Input - 9
+
+	       Output -                  9
+					               8 9 8
+					             7 8 9 8 7
+					           6 7 8 9 8 7 6
+					         5 6 7 8 9 8 7 6 5
+					       4 5 6 7 8 9 8 7 6 5 4
+					     3 4 5 6 7 8 9 8 7 6 5 4 3
+					   2 3 4 5 6 7 8 9 8 7 6 5 4 3 2
+					 1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
 
     	*/
 	}
