15ea75ab7831723ce9f6f21ccceb0f76ef686f3b
SDE Yash Agrawal <sde.yash.agrawal@gmail.com>
2024-02-22 12:31:07 +0530

    Shifting Zero Optimal Solution Added!

--- ShiftingZeros.java
+++ ShiftingZeros.java
@@ -15,6 +15,9 @@ class ShiftingZeros
 			arr[j] = s.nextInt();
 		}
 
+		//Optimal Solution
+		movingFront(arr);
+
 		int ind = 0, shift =0;
 		for(int i = 0; i<arr.length; i++)
 		{
@@ -35,6 +38,30 @@ class ShiftingZeros
 		for(int i: arr)
 		{
 			System.out.print(i + " ");
+		}
+	}
+
+	static void movingFront(int[] arr)
+	{
+		int counter = arr.length-1;
+		for(int i=arr.length-1; i>=0; i--)
+		{
+			if(arr[i] != 0)
+			{
+				arr[counter] = arr[i];
+				counter--;
+			}
+		}
+
+		while(counter >= 0)
+		{
+			arr[counter] = 0;
+			counter--;
+		}
+
+		for(int j=0; j<arr.length; j++)
+		{
+			System.out.print(arr[j] + ", ");
 		}
 	}
 }
